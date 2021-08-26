ba7a1bb421e258e2602a02d7f186ffe19cbce49d
Neda Habibi <nedahabibi28@gmail.com>
2021-08-26 15:30:12 +0100

    Add pagination.

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-export default function ListItem({item , onClick}){
-    return(
-        <li className='list-group-item' onClick = {onClick}>{item.login}</li>
+export default function ListItem({ item, onClick }) {
+    return (
+        <li className="list-group-item list-group-item-action">
+            <div className="row align-items-start" onClick={onClick}>
+                <div className="col">{item.login}</div>
+                <img className="col" className="img-thumbnail" style={{width:'60px',height:'60px'}} src={item.avatar_url}/>
+            </div>
+        </li>
     )
 }
